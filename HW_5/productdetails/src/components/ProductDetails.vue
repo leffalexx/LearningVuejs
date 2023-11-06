1867731fd6c9c3029568e3798ce184634eeb74c2
Aleksei <leffalexx@gmail.com>
2023-11-06 13:02:21 +0400

    HW5

---
+++ HW_5/productdetails/src/components/ProductDetails.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <img :src="product.image" />
+        <p>name: {{ product.name }}</p>
+        <p>price: {{ formattedPrice }}</p>
+        <p v-if="product.available">Available</p>
+        <p v-else>Out of stock</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+  data () {
+    return {
+      product:
+      {
+        image: require('../assets/Rexy.jpg'),
+        name: 'Porsche 911 RSR 1/18',
+        price: 99.99,
+        available: true
+      }
+    }
+  },
+  computed: {
+    formattedPrice: function () {
+      return `$${this.product.price}`
+    }
+  }
+}
+</script>
